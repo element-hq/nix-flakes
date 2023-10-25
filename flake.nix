118713d4c11e5ca9665f51d699435ae12ecbf2a2
Andrew Morgan <andrew@amorgan.xyz>
2023-10-25 15:46:30 +0100

    Add a composeShell fn
    This function can compose a single devenv devShell output from
    a list of known project names. This is useful for downstream
    flakes that would like to pull in multiple devenv devShells defined
    in this project (i.e. Synapse, Sytest, Complement)
    and combine them together.

--- flake.nix
+++ flake.nix
@@ -12,15 +12,28 @@
 
   outputs = inputs@{ devenv, nixpkgs, rust-overlay, systems, ... }:
     let
-      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+      # The local directory where project-specific flakes are stored.
       projectFlakesDirectory = ./project-flakes;
-    in
-    {
-      # We need to build a developer shell for each system type.
-      #
+
       # `forEachSystem` is a function that simply generates an attribute
       # set with each type of system as the keys, and the attribute set
       # defined below as the value.
+      #
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+
+      # Instantiate nixpkgs and inject the various available Rust versions
+      # as an overlay.
+      #
+      # mkPkgs is a function that builds an instance of nixpkgs with the
+      # given system.
+      overlays = [ (import rust-overlay) ];
+      mkPkgs = system:
+        import nixpkgs {
+          inherit system overlays;
+        };
+    in
+    {
+      # We want to build a developer shell for each system type.
       #
       # The result of the code below ends up looking like:
       # outputs = {
@@ -33,44 +46,59 @@
       #   };
       # }
       devShells = forEachSystem (system:
-        let
-          # Instantiate nixpkgs and inject the various available Rust versions
-          # as an overlay.
-          overlays = [ (import rust-overlay) ];
-          pkgs = import nixpkgs {
-            inherit system overlays;
+        # listToAttrs converts a list like:
+        #   [ {name = a; value = 1}, {name = b; value = 2;} ]
+        # into an attribute set:
+        #   { a = 1; b = 2; }
+        #
+        # We use this to create an attribute set like:
+        #  { synapse = devenv.lib.mkShell {...}; complement = devenv.lib.mkShell {...} }
+        builtins.listToAttrs (map (projectName: {
+          name = projectName;
+          value = devenv.lib.mkShell {
+            inherit inputs;
+            pkgs = mkPkgs system;
+            modules = [{
+              imports = [ "${projectFlakesDirectory}/${projectName}" ./common.nix ];
+            }];
           };
-        in
-          # listToAttrs converts a list in the form:
-          #   [ {name = a; value = 1}, {name = b; value = 2;} ]
-          # into an attribute set:
-          #   { a = 1; b = 2; }
-          #
-          # We use this to create an attribute set in the form:
-          #  { synapse = devenv.lib.mkShell {...}; complement = devenv.lib.mkShell {...} }
-          builtins.listToAttrs (map (projectName: {
-            name = projectName;
-            value = devenv.lib.mkShell {
-              inherit inputs pkgs;
-              modules = [{
-                imports = [
-                  # Build the shell for this project with the module provided by
-                  # the relevant file in $projectFlakesDirectory/$projectName.
-                  "${projectFlakesDirectory}/${projectName}"
-                  # ...and include the code from the common module.
-                  ./common.nix
-                ];
-              }];
-            };
-          # Read the directory names in $projectFlakeDirectory into a list.
-          # Note: using builtins.readDir is only allowed if `--impure` is
-          # passed to the `nix develop` invocation.
-          # devenv currently requires this, but eventually will not.
-          # See https://github.com/cachix/devenv/pull/745
-          #
-          # When that happens, we can modify this to be a hardcoded list of
-          # project names.
-          }) (builtins.attrNames (builtins.readDir projectFlakesDirectory )))
+        # Get the directory names in $projectFlakeDirectory as a list.
+        #
+        # Note: using builtins.readDir is only allowed if `--impure` is
+        # passed to the `nix develop` invocation.
+        # devenv currently requires this, but eventually will not.
+        # See https://github.com/cachix/devenv/pull/745
+        #
+        # When that happens, we could modify this to be a hardcoded list of
+        # project names, to avoid users needing to specify `--impure`.
+        }) (builtins.attrNames (builtins.readDir projectFlakesDirectory )))
+      );
+      # Define a flake output `composeShell`, which is a function that takes
+      # a list `projectNames`, and returns an attribute set of the default
+      # systems to a devenv shell with given project modules + the "common"
+      # module. This fits the expected value of the `devShells` flake output.
+      #
+      # This is useful for downstream flakes that want to combine multiple
+      # project flakes together into one shell (i.e. Synapse developers may
+      # also want the dependencies for the complement and sytest dev shells).
+      #
+      # For example:
+      #
+      #  outputs = {...}:
+      #   devShells = composeShell ["synapse" "complement" "sytest"];
+      composeShell = projectNames: forEachSystem (system:
+        {
+          default = devenv.lib.mkShell {
+            inherit inputs;
+            pkgs = mkPkgs system;
+            modules = map (projectName: {
+              imports = [
+                "${projectFlakesDirectory}/${projectName}"
+                ./common.nix
+              ];
+            }) projectNames;
+          };
+        }
       );
     };
 }
